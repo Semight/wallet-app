89526f6e32d4c818a2ddbfc9a97c22ec769d0b81
Semight <davidoluwasemiloorei@gmail.com>
2025-08-13 13:57:36 +0100

    feat: complete wallet app with mobile (Expo) and backend (Express) implementation

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,10 +3,12 @@ import SafeScreen from "@/components/SafeScreen";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { StatusBar } from "expo-status-bar";
+// import { CLERK_PUBLISHABLE_KEY } from "@/env" ;
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache}
+    publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
